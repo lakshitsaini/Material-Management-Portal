4c658e0a2453dd61b1c330398ca71045ae25f144
Sameer939 <67742155+Sameer939@users.noreply.github.com>
2020-10-02 15:45:06 +0530

    Update demo.py

--- Google-Drive-API/demo.py
+++ Google-Drive-API/demo.py
@@ -9,13 +9,13 @@ drive = GoogleDrive(gauth)
 
 fileId = ''
 def upload(filename):
-
     file1 = drive.CreateFile({'title' : filename,"parents": [{"kind": "drive#fileLink", "id": "1d7SPfSL1bQRicIpZWRc5EY_WaotFBF8r"}]})
     file1.SetContentFile('../Files/' + filename)
     # print('Uploaded!')
     file1.Upload()
     global fileId
     fileId = file1['id']
+    return fileId
 
 def getfile(id):
 	source = drive.CreateFile({'id': id})
@@ -28,4 +28,3 @@ def delete(id):
 def list(id):
     file = getfile(id)
     print('title: %s\n',(file['title']))
-
