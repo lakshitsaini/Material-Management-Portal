095aeb5b152a91125aa5ff22e1c8d3b4293b6f0a
Sameer939 <67742155+Sameer939@users.noreply.github.com>
2020-10-02 12:16:10 +0530

    Update demo.py

--- Google-Drive-API/demo.py
+++ Google-Drive-API/demo.py
@@ -6,7 +6,7 @@ gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
 
-file1 = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": "1d7SPfSL1bQRicIpZWRc5EY_WaotFBF8r"}]})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+file1 = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": "1d7SPfSL1bQRicIpZWRc5EY_WaotFBF8r"}]})
 file1.SetContentFile(file_name)
 
 file1.Upload()
