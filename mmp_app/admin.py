cebb5903a7140862e50b4d8b98479b90f90fb338
lakshitsaini <lakshitsaini2017@gmail.com>
2020-10-02 11:04:04 +0530

    temps added

--- mmp_app/admin.py
+++ mmp_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from mmp_app.models import Course,UploadFile,Student,Faculty
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(UploadFile)
+admin.site.register(Student)
+admin.site.register(Faculty)
