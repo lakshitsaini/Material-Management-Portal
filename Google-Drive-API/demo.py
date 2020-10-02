f8c7a185573ce05c874312821cfca24119d53942
Sameer939 <67742155+Sameer939@users.noreply.github.com>
2020-10-02 13:03:29 +0530

    Add files via upload

---
+++ Google-Drive-API/demo.py
@@ -0,0 +1,12 @@
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
+
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+file1 = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": "1d7SPfSL1bQRicIpZWRc5EY_WaotFBF8r"}]}) 
+file1.SetContentFile(file_name)
+
+file1.Upload()
